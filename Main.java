35824be6094ba934a275886c4d11121bcc5357e7
DirencanGider <direncangider@hotmail.com>
2024-01-18 15:15:08 +0300

    Commit Message - 18.01.2024 15:15

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-//PolyMorphism: Bir nesnenin birden farklı nesne gibi davranması
+/*
+PolyMorphism: Bir nesnenin birden farklı nesne gibi davranması
+Kuralımız; ana bir Class olmalı. Bu çalışmada oluşturduğumuz Hayvan classı gibi. Sonrasında diğer Kedi, Köpek ve At gibi
+subclasslara extend ediyoruz.
+Böylelikle Hayvan hayvan = new "Hayvan();" yerine "Kedi();" veya "Köpek();" veya da "At();" referanslarını yazdığımızda
+java bize problem çıkartmıyor. PolyMorphism'in önemi işte bu.
+ */
 class Hayvan {
     private String isim;
 
@@ -51,19 +57,35 @@ class At extends Hayvan {
     }
 }
 public class Main {
+
+    public static void konustur(Hayvan hayvan) { /*Aşağıdaki yorum satırındaki kodlara alternatif olarak
+                                                   bu şekilde bir fonksiyon yazabiliriz.
+                                                   Aşağıda da bu fonksiyonları çağırırken Hayvan class'ının
+                                                   içerisinde Kedi, Köpek ve At gibi referansları parantez
+                                                   içerisinde oluşturabiliriz.*/
+
+        System.out.println(hayvan.konus());
+
+    }
     public static void main(String[] args) {
 
+        /*
         Hayvan hayvan1 = new Kedi("Tekir"); /*"Hayvan("Limon");" demiştik en başta. Onu silip yerine
                                                    "Kedi("Tekir");" yazdık ve Java bir problem çıkarmadı.
                                                   Tekrar çalıştırdığımızda, İlk çalıştırmamızda "Hayvan konuşuyor..."
                                                   yazarken, Kedi olarak değiştirince "Tekir miyavlıyor..." yazdı.
-                                                  En son hayvan1 olarak değiştirdik. Diğer hayvanaları deneyeceğimizden*/
+                                                  En son hayvan1 olarak değiştirdik. Diğer hayvanaları deneyeceğimizden
         Hayvan hayvan2 = new Kopek("Karabaş");
         Hayvan hayvan3 = new At("Gülbatur");
 
         System.out.println(hayvan1.konus());
         System.out.println(hayvan2.konus());
         System.out.println(hayvan3.konus());
+        */
+
+        konustur(new Kedi("Tekir"));
+        konustur(new Kopek("Karabaş"));
+        konustur(new At("Gülbatur"));
 
 
 
